7f4d56cce96e2aee264002b03a84b706d78ef3ee
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2023-03-04 21:46:28 +0530

    Multiple values at one time using TestCase attribute

--- SparkyNUnitTest/CalculatorNUnitTests.cs
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -13,5 +13,31 @@ namespace SparkyNUnitTest
             int result = calculator.Add(1, 1);
             Assert.AreEqual(2, result);
         }
+
+        [Test]
+        public void IsOddNumber_InputEvenNumber_ReturnsFalse()
+        {
+            Calculator calculator = new();
+            bool result = calculator.IsOddNumber(2);
+
+            //Different methods to check boolean
+            //Assert.IsFalse(result);
+            
+            //Assert.That(result, Is.EqualTo(false));
+
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        [TestCase(5)]
+        [TestCase(3)]
+        [TestCase(7)]
+        [TestCase(11)]
+        public void IsOddNumber_InputOddNumber_ReturnsTrue(int number)
+        {
+            Calculator calculator = new();
+            bool result = calculator.IsOddNumber(number);
+            Assert.IsTrue(result);
+        }
     }
 }
