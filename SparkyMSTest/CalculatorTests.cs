9f1a3268e15908bf45cd9c3116a8ef891c4622ca
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2023-03-04 19:03:50 +0530

    Calculator MSTest, NUnit Project with scaffholding

---
+++ SparkyMSTest/CalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sparky;
+
+namespace SparkyMSTest
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Add_InputTwoNumbers_GetAddition()
+        {
+            Calculator calculator = new();
+            int result = calculator.Add(1, 1);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Sparky;
+
+namespace SparkyNUnitTest
+{
+    [TestFixture]
+    public class CalculatorNUnitTests
+    {
+        [Test]
+        public void Add_InputTwoIntegers_GetAddition()
+        {
+            Calculator calculator = new();
+            int result = calculator.Add(1, 1);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
